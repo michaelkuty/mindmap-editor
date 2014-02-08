ff29d08092cff2ba1e6f678327968af8edb51959
Michael Kutý <6du1ro.n@gmail.com>
2014-02-09 00:37:21 +0100

    static data verticle

--- mods/io.majklk~mindmapeditor~0.0.1/main.js
+++ mods/io.majklk~mindmapeditor~0.0.1/main.js
@@ -6,18 +6,23 @@ var config = container.config;
 //container.deployModule("io.vertx~mod-mailer~2.0.0-beta1", config);
 
 container.deployModule('io.vertx~mod-mongo-persistor~2.1.0', config.mongodb, 1, function(err, ID){
-	if (err) {
-		console.error(err)
-	}
+	if (!err) {
+    	load('utils/static_data.js');
+    	console.log("static data loaded")
+	} else {
+    err.printStackTrace();
+  	}
 });
 
 container.deployModule('io.vertx~mod-web-server~2.0.0-final', config.web_server, 1, function(err, ID){
 	if (err) {
-		console.error(err)
+		console.log(err)
 	}
 });
+/*
 container.deployModule('org.crashub~vertx.shell~2.0.4', config.shell, 1, function(err, ID){
 	if (err) {
 		console.error(err)
 	}
 });
+*/
---
+++ mods/io.majklk~mindmapeditor~0.0.1/utils/static_data.js
@@ -0,0 +1,49 @@
+var vertx = require('vertx')
+
+var eb = vertx.eventBus;
+
+var pa = 'vertx.mongopersistor';
+
+var maps = [
+  {
+  "_id": "1234-5678-9012-3456",
+  "name": "Berries",
+  "children": [
+      {
+      "key": "1",
+      "name": "Grapes",
+      "children": [
+        {
+        "key": "2",
+        "name": "Red grapes"
+        },
+        {
+        "key": "3",
+        "name": "Green grapes"
+        }
+      ]
+    },
+    {
+    "key": "4",
+    "name": "Strawberries"
+    },
+    {
+    "key": "5",
+    "name": "Blueberries"
+    }
+    ]
+  }
+];
+
+eb.send(pa, {action: 'delete', collection: 'mindmap', matcher: {}}, function(reply) {
+
+    for (var i = 0; i < maps.length; i++) {
+      eb.send(pa, {
+        action: 'save',
+        collection: 'mindmap',
+        document: maps[i]
+      });
+    }
+
+});
+
