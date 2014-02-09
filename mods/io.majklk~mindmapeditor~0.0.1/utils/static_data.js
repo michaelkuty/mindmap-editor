b4f2cccf4af51e5ff5e850ab6c95f155e2a02968
Michael Kutý <6du1ro.n@gmail.com>
2014-02-09 23:51:10 +0100

    new static data

--- mods/io.majklk~mindmapeditor~0.0.1/utils/static_data.js
+++ mods/io.majklk~mindmapeditor~0.0.1/utils/static_data.js
@@ -4,37 +4,56 @@ var eb = vertx.eventBus;
 
 var pa = 'vertx.mongopersistor';
 
-
-
-
-
 var treeData = [
   {
   "_id": "1234-5678-9012-3456",
-  "name": "Berries",
+  "name": "Vysoká škola",
+  "tags": ["škola", "myšlení"]
   "children": [
       {
       "key": "1",
-      "name": "Grapes",
+      "name": "BC",
+      "description": "example",
       "children": [
         {
-        "key": "2",
-        "name": "Red grapes"
+        "key": "AI",
+        "name": "Alikovaná informatika",
+        "description": "Student získá během studia dobré základní znalosti matematiky, algoritmizace a objektového 
+                        modelování, na které navazují předměty s přímou aplikací (zejména programování, návrhy 
+                        a správa informačních systémů a počítačových sítí, webovské technologie). Současně získává 
+                        základní znalosti ekonomie a managementu a povinně studuje odborný anglický jazyk. 
+                        U tohoto bakalářského studijního programu je kladen důraz na praktické dovednosti v oblasti 
+                        informačních technologií. Zejména jeho kombinovaná forma uspokojí stoupající poptávku 
+                        odborníků z jiných oblastí, kteří potřebují doplnit svoji odbornost o praktickou znalost 
+                        informačních technologií. 
+                        Uplatnění absolventa je především na pozicích programátora, návrháře a správce informačních 
+                        systémů a sítí, webdesignera, apod. Profil znalostí vyhovuje výkonu funkce manažera nižší až 
+                        střední úrovně, založení a vedení malé firmy a při dobrých jazykových předpokladech nachází 
+                        absolventi uplatnění také ve firmách s nadnárodní působností. 
+                        "
         },
         {
-        "key": "3",
-        "name": "Green grapes"
+        "key": "IM",
+        "name": "Informační management"
         }
-      ]
-    },
-    {
-    "key": "4",
-    "name": "Strawberries"
-    },
-    {
-    "key": "5",
-    "name": "Blueberries"
-    }
+      ]},
+      {
+      "key": "4",
+      "name": "ING",
+      "children": [
+        {
+        "key": "AI",
+        "name": "Alikovaná informatika"
+        },
+        {
+        "key": "IM",
+        "name": "Informační management"
+        }
+      ]},
+      {
+      "key": "PHD",
+      "name": "já nevím test utf-8"
+      }
     ]
   }
 ];
