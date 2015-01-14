e621aed61285bfd4fd704b9d52fb2074cd04b0c7
Michael Kuty <6du1ro.n@gmail.com>
2015-01-14 14:02:20 +0100

    fix

--- mods/io.majklk~mindmapeditor~0.0.1/main.js
+++ mods/io.majklk~mindmapeditor~0.0.1/main.js
@@ -12,21 +12,19 @@ for (var app in config) {
 		container.deployVerticle(app, app_config, app_config.workers, function(err, ID){
 			if (err) {
 				console.error(err.printStackTrace())
-			} else {
-    		    if (app.indexOf("mongo") > -1) {
-                    // load static data
-                    load('utils/static_data.js');
-                    console.log("Static data loaded.");             
-                }   
-			}
+			} else {}
 		});
 	} else {
-		//deployVerticle
+		//deployModule
 		container.deployModule(app, app_config, app_config.workers, function(err, ID){
 			if (err) {
 				console.error(err.printStackTrace());
 			} else {
-
+        	    if (app === "io.vertx~mod-mongo-persistor~2.1.0") {
+                    // load static data
+                    load('utils/static_data.js');
+                    console.log("Static data loaded.");             
+                }   
 			}
 		});
 	}
